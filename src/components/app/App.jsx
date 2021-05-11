3dd653bb8af0d951cade66b9b606e55820628122
SamuelHGerber <henrylightfoot@gmail.com>
2021-05-11 15:55:14 -0700

    router npm and setup

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,5 +1,30 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 
 export default function App() {
-  return <h1>Hello World</h1>;
+  return (
+    <Router>
+      <div>
+        <h1>Hello World</h1>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/characters">All Characters</Link>
+            </li>
+            <li>
+              <Link to="/detail">Character Detail</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/characters">
+            <CharactersList />
+          </Route>
+          <Route path="/detail">
+            <CharacterDetail />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
 }
